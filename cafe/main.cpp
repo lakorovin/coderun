6a2cc21dfe2ba778997e6816709c8efde32b7adc
Korovin Leo <lakorovin94@gmail.com>
2024-03-12 16:20:14 +0300

    cafe beautify

--- cafe/main.cpp
+++ cafe/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 #include <limits>
@@ -14,6 +13,40 @@ struct State {
     std::vector<int> days;
 };
 
+size_t find_best_ind(const std::vector<State>& k2, int limit) {
+    assert(!k2.empty());
+    size_t ind = 0;
+    for (; ind < k2.size() - 1; ++ind) {
+        if (k2[ind].amount >= limit) {
+            assert(ind == 0);
+            continue;
+        }
+        if (k2[ind].amount < k2[ind + 1].amount) {
+            break;
+        }
+    }
+    return ind;
+}
+
+void check_sanity(const std::vector<State>& k2, int limit) {
+    assert(!k2.empty());
+    for (size_t ind = 0; ind < k2.size() - 1; ++ind) {
+        if (k2[ind].amount >= limit) {
+            continue;
+        }
+        assert(k2[ind].amount <= k2[ind + 1].amount);
+    }
+}
+
+void print_result(const std::vector<State>& k2, size_t ind) {
+    std::cout << k2[ind].amount << std::endl;
+    std::cout << k2[ind].k1 << " " << k2[ind].k2 << std::endl;
+    for (int day : k2[ind].days) {
+        std::cout << day << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
     const int INF = 400 * 101;
@@ -22,7 +55,6 @@ int main()
     std::vector<State> k2;
     k2.reserve(N + 1);
     k2.push_back(State());
-    int sum = 0;
     for (int j = 0; j < N; ++j) {
         int x;
         std::cin >> x;
@@ -30,9 +62,7 @@ int main()
             std::vector<State> nextLev;
             nextLev.reserve(k2.size() + 1);
             State sPrev(-1, 0, INF);
-            for (int i = 0; i < k2.size() + 1; ++i) {
-                // State sCur = k2[i - 1];
-
+            for (size_t i = 0; i < k2.size() + 1; ++i) {
                 if ((i + 1 < k2.size()) && (sPrev.amount + x > k2[i + 1].amount)) {
                     State sK = k2[i + 1];
                     sK.k2 += 1;
@@ -40,7 +70,6 @@ int main()
                     sK.days.push_back(j + 1);
                     assert(sK.k1 == i);
                     nextLev.push_back(std::move(sK));
-                    // k2[i].k1 = sK.k1 - 1;
                 }
                 else {
                     sPrev.amount += x;
@@ -52,11 +81,10 @@ int main()
                     sPrev = k2[i];
                 }
             }
-            k2 = nextLev;
-
+            k2 = std::move(nextLev);
         }
         else {
-            for (int i = 0; i < k2.size(); ++i) {
+            for (size_t i = 0; i < k2.size(); ++i) {
                 State& sBuy = k2[i];
                 if ((i + 1 < k2.size()) && (sBuy.amount + x > k2[i + 1].amount)) {
                     State sK = k2[i + 1];
@@ -73,34 +101,9 @@ int main()
         }
     }
 
-    for (int ind = 0; ind < k2.size() - 1; ++ind) {
-        if (k2[ind].amount >= INF) {
-            // assert(ind < 2);
-            continue;
-        }
-        assert(k2[ind].amount <= k2[ind + 1].amount);
-    }
-
-
-    int ind = 0;
-    for (; ind < k2.size() - 1; ++ind) {
-        if (k2[ind].amount >= INF) {
-            assert(ind == 0);
-            continue;
-        }
-        if (k2[ind].amount < k2[ind + 1].amount) {
-            break;
-        }
-    }
-
-
-    std::cout << k2[ind].amount << std::endl;
-    std::cout << k2[ind].k1 << " " << k2[ind].k2 << std::endl;
-    for (int day : k2[ind].days) {
-        std::cout << day << " ";
-    }
-    std::cout << std::endl;
-
+    check_sanity(k2, INF);
+    size_t ind = find_best_ind(k2, INF);
+    print_result(k2, ind);
     return 0;
 }
 
