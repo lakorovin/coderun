5fbbe3d67530fa7bd810175771df9fab9818209a
Korovin Leo <lakorovin94@gmail.com>
2024-03-24 00:32:10 +0300

    sizif blank

---
+++ sizif/main.cpp
@@ -0,0 +1,27 @@
+// https://contest.yandex.ru/contest/36783/problems/H/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+long long getEnergyForUnion(const vector<int>& stones) {
+    // your code goes here
+    return 0;
+}
+
+vector<int> readList(int n) {
+    vector<int> res(n);
+    for (int i = 0; i < n; i++) {
+        cin >> res[i];
+    }
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> stones = readList(n);
+
+    cout << getEnergyForUnion(stones);
+}
