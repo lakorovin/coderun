54a27d9ffdae44044c42b26e898c36c3ef7fd580
Korovin Leo <lakorovin94@gmail.com>
2024-03-24 00:44:57 +0300

    sizif

--- sizif/main.cpp
+++ sizif/main.cpp
@@ -2,16 +2,29 @@
 
 #include <vector>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-long long getEnergyForUnion(const vector<int>& stones) {
-    // your code goes here
-    return 0;
+long long getEnergyForUnion(vector<long long> stones) {
+    if (stones.size() < 1) {
+        return 0;
+    }
+    std::priority_queue minq2(stones.begin(), stones.end(), std::greater<long long>());
+    long long work = 0;
+    while (minq2.size() > 1) {
+        long long l1 = minq2.top();
+        minq2.pop();
+        long long l2 = minq2.top();
+        work += l1 + l2;
+        minq2.pop();
+        minq2.push(l2 + l1);
+    }
+    return work;
 }
 
-vector<int> readList(int n) {
-    vector<int> res(n);
+vector<long long> readList(int n) {
+    vector<long long> res(n);
     for (int i = 0; i < n; i++) {
         cin >> res[i];
     }
@@ -21,7 +34,6 @@ vector<int> readList(int n) {
 int main() {
     int n;
     cin >> n;
-    vector<int> stones = readList(n);
-
-    cout << getEnergyForUnion(stones);
+    vector<long long> stones = readList(n);
+    cout << getEnergyForUnion(std::move(stones));
 }
