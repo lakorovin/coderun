b388fb68990497b5e727051ec4e32c02bdbfc5ba
Korovin Leo <lakorovin94@gmail.com>
2024-03-24 14:27:26 +0300

    queens pass

--- queens/main.cpp
+++ queens/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <numeric>
 #include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
@@ -14,35 +15,41 @@ void printArray(const vector<int>& combination) {
     cout << endl;
 }
 
-bool is_good(const vector<int>& places) {
-    int n = (int)places.size();
-    vector<int> diag1(2*n-1);
-    vector<int> diag2(2 * n - 1);
-    for (int i = 0; i < n; ++i) {
-        int d1 = i + places[i];
-        int d2 = n - 1 + i - places[i];
+void fill_rec(unordered_set<int>& pl_set, vector<vector<int>>& res, vector<int>& diag1, vector<int>& diag2, int line, int n, vector<int>& places) {
+    if (line >= n) {
+        res.push_back(places);
+        return;
+    }
+    for (int v : pl_set) {
+        places[line] = v;
+        int d1 = line + v;
+        int d2 = n - 1 + line - v;
         if (diag1[d1]) {
-            return false;
+            continue;
         }
         if (diag2[d2]) {
-            return false;
+            continue;
         }
         diag1[d1] = 1;
         diag2[d2] = 1;
+        unordered_set<int> pl_set2(pl_set.begin(), pl_set.end());
+        pl_set2.erase(v);
+        fill_rec(pl_set2, res, diag1, diag2, line + 1, n, places);
+        diag1[d1] = 0;
+        diag2[d2] = 0;
     }
-    return true;
 }
 
 vector<vector<int>> getAllPeacefulCombinations(int n) {
     vector<vector<int>> res;
     vector<int> places(n);
     std::iota(places.begin(), places.end(), 0);
-    do {
-        if (is_good(places)) {
-            res.push_back(places);
-        }
-    }
-    while (std::next_permutation(places.begin(), places.end()));
+    vector<int> diag1(2 * n - 1);
+    vector<int> diag2(2 * n - 1);
+
+    vector<int> places2(n, -1);
+    unordered_set<int> pl_set(places.begin(), places.end());
+    fill_rec(pl_set, res, diag1, diag2, 0, n, places2);
     return res;
 }
 
