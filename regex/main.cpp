f5cd99c68050ae52f1192d88b7baa8ce4a04062f
Korovin Leo <lakorovin94@gmail.com>
2024-03-25 21:22:29 +0300

    regex v2 (without regex)

--- regex/main.cpp
+++ regex/main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-bool stringMatchesTemplate(string stringToCheck, string templateString) {
+bool stringMatchesTemplate2(string stringToCheck, string templateString) {
     string tm;
     tm.reserve(2 * templateString.size());
     for (char c : templateString) {
@@ -26,6 +26,80 @@ bool stringMatchesTemplate(string stringToCheck, string templateString) {
     // your code goes here
     return std::regex_match(stringToCheck, txt_regex);
 }
+
+string normalizeTemplate(string templateString) {
+    const int CHAR = 1;
+    const int STAR = 2;
+    int state = CHAR;
+
+    string tm;
+    tm.reserve(templateString.size());
+
+    for (char c : templateString) {
+        if (c == '*') {
+            state = STAR;
+        } else if (c == '?') {
+            tm.push_back(c);
+        }
+        else {
+            if (state == STAR) {
+                tm.push_back('*');
+            }
+            state = CHAR;
+            tm.push_back(c);
+        }
+    }
+    if (state == STAR) {
+        tm.push_back('*');
+    }
+    return tm;
+}
+
+bool rec_check(const string& stringToCheck, string& templateString, int sind, int tind) {
+    if (sind >= stringToCheck.size() && tind >= templateString.size()) {
+        return true;
+    }
+    if (sind >= stringToCheck.size() && (tind + 1 == templateString.size()) && templateString[tind] == '*') {
+        return true;
+    }
+    if (sind >= stringToCheck.size()) {
+        return false;
+    }
+    if (tind + 1 == templateString.size() && templateString[tind] == '*') {
+        return true;
+    }
+    if (tind >= templateString.size()) {
+        return false;
+    }
+    char curT = templateString[tind];
+    if (curT == '?') {
+        return rec_check(stringToCheck, templateString, sind + 1, tind + 1);
+    } else if (curT == '*') {
+        for (int i = 0; i + sind < stringToCheck.size(); ++i) {
+            bool maybe = rec_check(stringToCheck, templateString, sind + i, tind + 1);
+            if (maybe) {
+                return true;
+            }
+        }
+        return false;
+    }
+    else {
+        if (curT != stringToCheck[sind]) {
+            return false;
+        }
+        return rec_check(stringToCheck, templateString, sind + 1, tind + 1);
+    }
+}
+
+bool stringMatchesTemplate(string stringToCheck, string templateString) {
+    string tm = normalizeTemplate(std::move(templateString));
+    if (tm.size() > stringToCheck.size() * 2) {
+        return false;
+    }
+    // your code goes here
+    return rec_check(stringToCheck, tm, 0, 0);
+}
+
 
 int main() {
     string templateString;
