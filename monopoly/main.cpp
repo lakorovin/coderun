4fdb0758e415fa96439ad47a92ff143ce2c9754e
Korovin Leo <lakorovin94@gmail.com>
2024-03-26 04:39:49 +0300

    monopoly

--- monopoly/main.cpp
+++ monopoly/main.cpp
@@ -1,8 +1,11 @@
 // https://contest.yandex.ru/contest/36783/problems/O/
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <map>
+#include <cassert>
+#include <set>
 
 using namespace std;
 
@@ -17,8 +20,63 @@ struct Building {
 
 
 long long getMaxFinalCapital(vector<Building> buildings, int startCapital, int maxNumberOfBuildings) {
-    // your code goes here
-    return 0;
+    long long res = startCapital;
+    std::sort(buildings.begin(), buildings.end(), 
+        [](const auto& a, const auto& b) { return std::tie(b.addedCapital, a.needCapital) < std::tie(a.addedCapital, b.needCapital); });
+    int cur = 0;
+    vector<int> visited(buildings.size(), 0);
+    map<long long, int> cost_to_index;
+    set<int> unseen;
+    int pure_unseen = 0;
+    while(cur < (int)buildings.size()) {
+        const Building& b = buildings[cur];
+        if (b.needCapital <= res) {
+            assert(visited[cur] == 0);
+            visited[cur] = 1;
+            --maxNumberOfBuildings;
+            res += b.addedCapital;
+            if (cur == pure_unseen) {
+                ++pure_unseen;
+            }
+            if (maxNumberOfBuildings == 0) {
+                break;
+            }
+            unseen.erase(cur);
+            if (!cost_to_index.empty()) {
+                auto it = cost_to_index.upper_bound(res);
+                if (it == cost_to_index.begin()) {
+                    cur = pure_unseen;
+                }
+                else {
+                    --it;
+                    cur = it->second;
+                    cost_to_index.erase(it);
+                }
+            }
+            else {
+                if (unseen.empty()) {
+                    cur = pure_unseen;
+                }
+                else {
+                    cur = *unseen.cbegin();
+                }
+            }
+        }
+        else {
+            if (cost_to_index.empty() || cost_to_index.cbegin()->first > b.needCapital) {
+                unseen.erase(cur);
+                cost_to_index[b.needCapital] = cur;
+            }
+            else {
+                unseen.insert(cur);
+            }
+            if (cur == pure_unseen) {
+                ++pure_unseen;
+            }
+            cur = pure_unseen;
+        }
+    }
+    return res;
 }
 
 
