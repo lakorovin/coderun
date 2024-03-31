c7bbf2f23d1cf30d06aaf8324ac33af4e6bf48d1
Korovin Leo <lakorovin94@gmail.com>
2024-03-31 03:33:43 +0300

    monopoly 2 queue solution

--- monopoly/main.cpp
+++ monopoly/main.cpp
@@ -1,103 +1,68 @@
 // https://contest.yandex.ru/contest/36783/problems/O/
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <map>
-#include <tuple>
-#include <set>
-
-using namespace std;
+#include <queue>
 
 
 struct Building {
     int needCapital;
     int addedCapital;
 
-    Building(int c, int p): needCapital(c), addedCapital(p){
+    Building(int c, int p) : needCapital(c), addedCapital(p) {
     }
 };
 
+int readInt() {
+    int x;
+    std::cin >> x;
+    return x;
+}
 
-long long getMaxFinalCapital(vector<Building> buildings, int startCapital, int maxNumberOfBuildings) {
+
+long long getMaxFinalCapital(std::vector<Building> buildings, int startCapital, int maxNumberOfBuildings) {
     long long res = startCapital;
-    std::sort(buildings.begin(), buildings.end(),
-        [](const auto& a, const auto& b) { return std::tie(b.addedCapital, a.needCapital) < std::tie(a.addedCapital, b.needCapital); });
-    map<long long, int> cost_to_index;
-    set<int> unseen;
-    int cur = 0;
-    int pure_unseen = 0;
-    while(cur < (int)buildings.size()) {
-        if (cur == pure_unseen) {
-            ++pure_unseen;
-        }
-        const Building& b = buildings[cur];
-        if (b.needCapital <= res) {
-            --maxNumberOfBuildings;
-            res += b.addedCapital;
-            if (maxNumberOfBuildings == 0) {
-                break;
-            }
-            unseen.erase(cur);
-            if (!cost_to_index.empty()) {
-                auto it = cost_to_index.upper_bound(res);
-                if (it == cost_to_index.begin()) {
-                    cur = pure_unseen;
-                }
-                else {
-                    --it;
-                    cur = it->second;
-                    cost_to_index.erase(it);
-                }
-            }
-            else {
-                if (unseen.empty()) {
-                    cur = pure_unseen;
-                }
-                else {
-                    cur = *unseen.cbegin();
-                }
-            }
-        }
-        else {
-            if (cost_to_index.empty() || cost_to_index.cbegin()->first > b.needCapital) {
-                unseen.erase(cur);
-                cost_to_index[b.needCapital] = cur;
-                if (unseen.empty() || !cost_to_index.empty()) {
-                    cur = pure_unseen;
-                }
-                else {
-                    cur = *unseen.cbegin();
-                }
-            }
-            else {
-                unseen.insert(cur);
-                cur = pure_unseen;
-            }
+    auto cmpNeed = [](const Building& a, const Building& b) {
+        return a.needCapital > b.needCapital;
+        };
 
+    auto cmpAdd = [](const Building& a, const Building& b) {
+        return a.addedCapital < b.addedCapital;
+        };
+    std::priority_queue<Building, std::vector<Building>, decltype(cmpNeed)> tr(cmpNeed, std::move(buildings));
+    std::priority_queue<Building, std::vector<Building>, decltype(cmpAdd)> best(cmpAdd, std::vector<Building>());
+
+    while (maxNumberOfBuildings > 0) {
+        while (!tr.empty() && tr.top().needCapital <= res) {
+            best.push(tr.top());
+            tr.pop();
         }
+        if (best.empty()) {
+            break;
+        }
+        res += best.top().addedCapital;
+        best.pop();
+        --maxNumberOfBuildings;
     }
     return res;
 }
 
 
-vector<Building> readBuildings(int n) {
-    vector<Building> buildings;
+std::vector<Building> readBuildings(int n) {
+    std::vector<Building> buildings;
     for (int i = 0; i < n; i++) {
         int c, p;
-        cin >> c >> p;
+        std::cin >> c >> p;
         buildings.emplace_back(c, p);
     }
     return buildings;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    int k;
-    cin >> k;
-    vector<Building> buildings = readBuildings(n);
-    int M;
-    cin >> M;
-    cout << getMaxFinalCapital(std::move(buildings), M, k) << endl;
+    int n = readInt();
+    int k = readInt();
+    std::vector<Building> buildings = readBuildings(n);
+    int M = readInt();
+    std::cout << getMaxFinalCapital(std::move(buildings), M, k) << std::endl;
+    return 0;
 }
